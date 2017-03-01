c486f265080bd6a77e57898f4141e1da62ce9b2e
Steven Tran <stevemt1@uci.edu>
2017-02-28 21:27:18 -0800

    Edited Game Scene to include the Spawners, Cursors, and simple Moving minions on button press

---
+++ GirlSquadTowerDefense_Steven/Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public bool player1;
+	public float speed;
+	public int health;
+
+	void Start()
+	{
+		if (transform.position.x < 0)
+			player1 = true;
+		else
+			player1 = false;
+	}
+
+
+	void Update () 
+	{
+		Movement ();
+	}
+		
+	void Movement()
+	{
+		if (player1) 
+		{
+			transform.position += new Vector3 (speed * Time.deltaTime, 0.0f, 0.0f);
+		} 
+		else
+			transform.position -= new Vector3 (speed * Time.deltaTime, 0.0f, 0.0f);
+	}
+
+}
---
+++ GirlSquadTowerDefense_Steven/Assets/Scripts/GameManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public GameObject leftCursor;
+	public GameObject rightCursor;
+	public GameObject enemy;
+
+
+
+	public int player1lane = 1;
+	public int player2lane = 1;
+
+
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+	}
+
+	void Start()
+	{
+		leftCursor = Instantiate (leftCursor);
+		rightCursor = Instantiate (rightCursor);
+
+	}
+
+
+	void Update()
+	{	
+		if (Input.GetKeyDown (KeyCode.LeftShift)) 
+		{
+			MoveCursor (leftCursor);
+			if (player1lane == 3)
+				player1lane = 1;
+			else
+				player1lane += 1;
+		}
+		if (Input.GetKeyDown (KeyCode.RightShift)) 
+		{
+			MoveCursor (rightCursor);
+			if (player2lane == 3)
+				player2lane = 1;
+			else
+				player2lane += 1;
+		}
+
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			SpawnEnemy (leftCursor.transform.localPosition + new Vector3(1f, 0f, 0f));
+		}
+		if (Input.GetKeyDown (KeyCode.J)) 
+		{
+			SpawnEnemy (rightCursor.transform.localPosition - new Vector3(1f, 0f, 0f));
+
+		}
+
+	}
+
+	void MoveCursor(GameObject Cursor)
+	{
+		if (Cursor.gameObject.transform.localPosition.y == -3.5f)
+			Cursor.gameObject.transform.localPosition += new Vector3 (0f, 7f, 0f);
+		else 
+			Cursor.gameObject.transform.localPosition -= new Vector3 (0f, 3.5f, 0f);
+	}
+
+	void SpawnEnemy(Vector3 spawnPosition)
+	{
+		Instantiate (enemy, spawnPosition, Quaternion.identity);
+	}
+		
+
+}
